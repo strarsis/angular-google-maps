8672db73bf33153f43d879827fbf8a40e2ec8809
strarsis <strarsis@gmail.com>
2022-03-30 22:00:19 +0200

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [BrowserModule, ReactiveFormsModule],
+  imports: [BrowserModule, ReactiveFormsModule, GoogleMapsModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
